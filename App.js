adcc082e8cc7bb6e57a0cc2c6cb48fefe41df975
LakshmiP10 <lakshmi.uitech@gmail.com>
2024-04-11 22:05:17 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,12 @@
+const parent = React.createElement("div",{id:"parent"},[
+    React.createElement("div",{id:"child"},
+    [React.createElement("h1",{},"h1 tag from  div child"),
+    React.createElement("h2",{},"h2 tag from child div")]),
+    React.createElement("div",{id:"child2"},
+    [React.createElement("h1",{},"h1 tag from  div child2"),
+    React.createElement("h2",{},"h2 tag from div child2 ")])
+
+]);
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent)
