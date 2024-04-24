6c57f677a992e580618fff7288e9127944d04998
LakshmiP10 <lakshmi.uitech@gmail.com>
2024-04-24 17:59:49 +0530

    added react, react-dom,parcel npx used,browserslist

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const parent = React.createElement("div",{id:"parent"},[
     React.createElement("div",{id:"child"},
-    [React.createElement("h1",{},"h1 tag from  div child"),
-    React.createElement("h2",{},"h2 tag from child div")]),
+    [React.createElement("h1",{},"from namaste React"),
+    React.createElement("h2",{},"by Lakshmi")]),
     React.createElement("div",{id:"child2"},
     [React.createElement("h1",{},"h1 tag from  div child2"),
     React.createElement("h2",{},"h2 tag from div child2 ")])
